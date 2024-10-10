8c8a3ead480b11adde84a5db7e644cf507ce3153
froustex <stephane-feroussier_student2022@wilder.school>
2024-10-10 16:38:48 +0200

    modification CV

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import CV from '../assets/CV.pdf'
+import React from "react";
+import CV from "../assets/CVStef.pdf";
 
 const CTA = () => {
-    return (
-        <div className='cta'>
-            <a href={CV} download  className='btn'>Download CV</a>
-            <a href="#contact" className='btn btn-primary'>Contact</a>
-        </div>
-    );
+  return (
+    <div className="cta">
+      <a href={CV} download className="btn">
+        Download CV
+      </a>
+      <a href="#contact" className="btn btn-primary">
+        Contact
+      </a>
+    </div>
+  );
 };
 
 export default CTA;
